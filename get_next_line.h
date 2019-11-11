335bde304c0a3820adcab00813e9ea3a97b2f626
Romain Laforgue <rolaforg@z2r6p2.le-101.fr>
2019-11-11 18:10:49 +0100

    Initial commit

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   get_next_line.h                                  .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: rolaforg <rolaforg@student.le-101.fr>      +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2019/11/11 15:56:46 by rolaforg     #+#   ##    ##    #+#       */
+/*   Updated: 2019/11/11 17:16:04 by rolaforg    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <unistd.h>
+
+int	get_next_line(int fd, char **line);
+
+#endif
