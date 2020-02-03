64392fdfa7e2c804ec9bde19e15b7285a27ea0d1
romslf <rom.laforgue@gmail.com>
2020-02-03 20:04:56 +0100

    Normed

--- get_next_line.c
+++ get_next_line.c
@@ -6,62 +6,62 @@
 /*   By: rolaforg <rolaforg@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/11/23 13:36:14 by rolaforg     #+#   ##    ##    #+#       */
-/*   Updated: 2020/02/01 18:17:28 by rolaforg    ###    #+. /#+    ###.fr     */
+/*   Updated: 2020/02/03 20:00:19 by rolaforg    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char        *ft_substrdup(char *s, unsigned int start, size_t len)
+char	*ft_substrdup(char *s, unsigned int start, size_t len)
 {
-    size_t  count;
-    size_t  size;
-    char    *tab;
+	size_t	count;
+	size_t	size;
+	char	*tab;
 
-    count = 0;
-    if (!s)
-        return (NULL);
-    if (ft_strlen(s) < start)
-        return (NULL);
-    size = ft_strlen(s + start);
-    if (size < len)
-        len = size;
-    if (!(tab = (char *)malloc((len + 1) * sizeof(char))))
-        return (NULL);
-    while (count < len)
-    {
-        tab[count] = s[start + count];
-        count++;
-    }
-    tab[count] = '\0';
+	count = 0;
+	if (!s)
+		return (NULL);
+	if (ft_strlen(s) < start)
+		return (NULL);
+	size = ft_strlen(s + start);
+	if (size < len)
+		len = size;
+	if (!(tab = (char *)malloc((len + 1) * sizeof(char))))
+		return (NULL);
+	while (count < len)
+	{
+		tab[count] = s[start + count];
+		count++;
+	}
+	tab[count] = '\0';
 	free(s);
-    return (tab);
+	return (tab);
 }
 
-char        *ft_substr(char *s, unsigned int start, size_t len)
+char	*ft_substr(char *s, unsigned int start, size_t len)
 {
-    size_t  count;
-    size_t  size;
-    char    *tab;
+	size_t	count;
+	size_t	size;
+	char	*tab;
 
-    count = 0;
-    if (!s)
-        return (NULL);
-    if (ft_strlen(s) < start)
-        return (NULL);
-    size = ft_strlen(s + start);
-    if (size < len)
-        len = size;
-    if (!(tab = (char *)malloc((len + 1) * sizeof(char))))
-        return (NULL);
-    while (count < len)
-    {
-        tab[count] = s[start + count];
-        count++;
-    }
-    tab[count] = '\0';
-    return (tab);
+	count = 0;
+	if (!s)
+		return (NULL);
+	if (ft_strlen(s) < start)
+		return (NULL);
+	size = ft_strlen(s + start);
+	if (size < len)
+		len = size;
+	if (!(tab = (char *)malloc((len + 1) * sizeof(char))))
+		return (NULL);
+	while (count < len)
+	{
+		tab[count] = s[start + count];
+		count++;
+	}
+	tab[count] = '\0';
+	return (tab);
 }
 
 void	*ft_calloc(size_t count, size_t size)
@@ -77,7 +77,6 @@ void	*ft_calloc(size_t count, size_t size)
 	return (p);
 }
 
-
 int		ft_extract(char **line, char **tmp, int index)
 {
 	*line = ft_substr(*tmp, 0, index);
@@ -87,35 +86,28 @@ int		ft_extract(char **line, char **tmp, int index)
 
 int		get_next_line(int fd, char **line)
 {
-    char		buffer[BUFFER_SIZE + 1];
+	char		buffer[BUFFER_SIZE + 1];
 	static char	*tmp;
-	int			readCnt;
+	int			readcnt;
 	int			index;
 
-	if (!line || BUFFER_SIZE <= 0 || (readCnt = read(fd, 0, 0)) < 0)
-	{
-		if (tmp)
-		{
-			free(tmp);
-			tmp = NULL;
-		}
+	if (!line || BUFFER_SIZE <= 0 || (readcnt = read(fd, 0, 0)) < 0)
 		return (-1);
-	}
 	if (!tmp)
 	{
 		if (!(tmp = malloc(sizeof(char))))
 			return (-1);
 		tmp[0] = '\0';
 	}
-	readCnt = 1;
-	while (!(index = ft_linelen(tmp)) && (readCnt = read(fd, buffer, BUFFER_SIZE)) != 0)
+	readcnt = 1;
+	while (!(index = ft_linelen(tmp)) && (readcnt = read(fd, buffer, BUFFER_SIZE)) != 0)
 	{
-		buffer[readCnt] = '\0';
+		buffer[readcnt] = '\0';
 		tmp = ft_strjoin(tmp, buffer);
 	}
 	if (index < 0)
 		index = 0;
-	if (readCnt == 0)
+	if (readcnt == 0)
 	{
 		*line = ft_substr(tmp, 0, ft_strlen(tmp));
 		free(tmp);
@@ -124,55 +116,3 @@ int		get_next_line(int fd, char **line)
 	}
 	return (ft_extract(line, &tmp, index));
 }
-
-// int        main(int ac, char **av)
-// {
-//     int        fd;
-//     int        i;
-//     char       *line;
-//     int        j;
-//     int        ret;
-
-//     line = 0;
-//     ret = 0;
-//     ac = (int)ac;
-//     av = (char**)av;
-//     j = 0;
-//     if (ac == 2)
-//         fd = open(av[1], O_RDONLY);
-//     else
-//         fd = 1;
-
-//     while ((ret = get_next_line(fd, &line)))
-//     {
-//         i = 0;
-//         if (ret == -1)
-//         {
-//             free(line);
-//             return (0);
-//         }
-//         while (line[i])
-//         {
-//             write(1, &line[i], 1);
-//             i++;
-//         }
-//         write(1, "\n", 1);
-//         j++;
-//         free(line);
-//     }
-//     i = 0;
-// if (line && line[i])
-//     {
-//         while (line[i])
-//         {
-//             write(1, &line[i], 1);
-//             i++;
-//         }
-//         write(1, "\n", 1);
-//         free(line);
-//     }
-// else
-//     free(line);
-//     dprintf(1, "LIGNES LUES = %d\n", j);
-//     return (0);
-// }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: rolaforg <rolaforg@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/11/23 15:54:39 by rolaforg     #+#   ##    ##    #+#       */
-/*   Updated: 2020/02/01 18:03:08 by rolaforg    ###    #+. /#+    ###.fr     */
+/*   Updated: 2020/02/03 19:59:22 by rolaforg    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -22,10 +22,11 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	i = 0;
 	x = 0;
 	if (!s1)
-		return(ft_substr((char *)s2, 0, ft_linelen((char *)s2)));
+		return (ft_substr((char *)s2, 0, ft_linelen((char *)s2)));
 	if (!s2)
 		return (NULL);
-	if (!(res = malloc(sizeof(char) * (ft_strlen((char *)s1) + ft_strlen((char *)s2) + 1))))
+	if (!(res = malloc(sizeof(char) * (ft_strlen((char *)s1) +
+		ft_strlen((char *)s2) + 1))))
 		return (NULL);
 	while (s1[i])
 		res[x++] = s1[i++];
@@ -39,9 +40,9 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 char	*ft_memchr(char *s, int c, size_t n)
 {
-	unsigned char		chr;
-	char	*str;
-	
+	unsigned char	chr;
+	char			*str;
+
 	if (s && n)
 	{
 		str = s;
@@ -64,7 +65,7 @@ char	*ft_memchr(char *s, int c, size_t n)
 size_t	ft_strlen(char *str)
 {
 	size_t	i;
-	
+
 	if (!str)
 		return (0);
 	i = 0;
